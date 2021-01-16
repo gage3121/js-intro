e217dedd1cdb3336578140f34cc9ca619a5451fc
gage3121 <gagenhall@gmail.com>
2021-01-16 11:13:06 -0600

    Practiced javascript

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("Suh dude")
+//console.log("Suh dude")
+
+let firstName = "Beyonce";
+let lastName = "Knowles";
+
+console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+
+firstName = "Gage"
+lastName = "Hall"
+
+console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+
+//const favoriteFood = "Meatloaf"
+
+//START DATA TYPES
+
+const dog = "bark";
+console.log(typeof dog)
+
+const answer = 33;
+const negative = -14;
+
+console.log(typeof answer, typeof negative);
+
+const iLoveJavaScript = true;
+
+console.log(typeof iLoveJavaScript);
